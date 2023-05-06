7653f1add662ac2eefffac8ac1eb415f94f4e003
Jothiswaran A <53011406+devjothish@users.noreply.github.com>
2023-05-07 03:32:26 +0530

    Add files via upload

---
+++ Easy/MissingNumber.java
@@ -0,0 +1,17 @@
+package Easy;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+   
+    public static void main(String[] args){
+
+        int arr[]={3,0,1};
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=i){
+                System.out.println(i);
+            }
+        }
+    }
+}

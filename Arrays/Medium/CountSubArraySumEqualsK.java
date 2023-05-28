497475edde40844fa47c714eb0246c1ab4208f7f
Jothiswaran A <jothiswaran2604@gmail.com>
2023-05-29 00:16:21 +0530

    Commit-May 29

---
+++ Arrays/Medium/CountSubArraySumEqualsK.java
@@ -0,0 +1,27 @@
+package Arrays.Medium;
+
+public class CountSubArraySumEqualsK {
+   //better approach - O(n^2), O(1)
+    public static int FindAllSubArraysWithGivenSum(int [] arr, int k){
+       int count=0;
+       for(int i=0;i<arr.length;i++){
+           int sum=0;
+           for(int j=i;j<arr.length;j++){
+               sum+=arr[j];
+               if(sum==k){
+                   count++;
+               }
+           }
+       }
+       return count;
+    }
+
+    //Optimalapproach - O(n)
+
+
+    public static void main(String[] args) {
+        int[] arr={3,1,2,4};
+        int k=6;
+        System.out.println(FindAllSubArraysWithGivenSum(arr,k));
+    }
+}
